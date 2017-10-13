19c20e34834a402c852b333f7b62453f2b7661c1
phamducminh2895 <phamducminh2895@gmail.com>
2017-10-13 17:35:56 +0700

    Thêm folder

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Autumn Coderetreat 2017</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+</head>
+<body>
+<div class="container1">
+    <div class="row page-heading">
+        <div>
+            <img src="images/page-header.png" class="page-header-image"/>
+        </div>
+        <img src="images/bg0.png" style="margin: -170px 640px 0 0">
+        <img src="images/bg1.png" style="margin: -10px 640px 0 0">
+    </div>
+    <div class="row page-content">
+    <div class="row page-banner">
+            <div class="row">
+                <div>
+                    <h3 style="text-align: center">Đơn vị tổ chức</h3>
+                    <hr>
+                    <br><br>
+                    <img src="images/logo.png" style="width: 80%; height: 80%; margin-left: 10%"/>
+                    <br><br>
+                    <p class="fa fa-calendar fa-lg" style="text-align: center"> 8:00 - 16:00, Chủ Nhật, Ngày 10/9/2017</p>
+                    <p class="fa fa-map-marker fa-lg" style="text-align: justify; line-height: 30px"> VP Agilead - Tầng 11, Tòa nhà MD Complex, 68 Nguyễn Cơ Thạch, Hà Nội</p>
+                    <center><a href="#" target="_blank" class="button">Đăng ký tham dự</a></center>
+                </div>
+            </div>
+    </div>
+        <img src="images/bg3.png" style="margin-top: 80px">
+    </div>
+
+</div>
+<div class="container2">
+    <img src="images/bg4.png" style="margin: -15px 0 0 0">
+        <div class="introduction" style="margin-top: -460px ">
+            <div class="row">
+                <img src="images/Coderetreat.png" class="coderetreat"/>
+            </div>
+            <div class="introduction-text">
+                <p>Coderetreat là một sự kiện thực hành lập trình chuyên sâu, các hoạt động tập trung vào những nguyên tắc cơ bản của phát triển phần mềm và thiết kế. Tại sự kiện, các nhà phát triển có cơ hội tham gia vào thực hành có chủ ý (deliberate practice\deep practice), tránh xa những áp lực phải ‘hoàn thành nhiệm vụ’ trong công việc hằng ngày. Hoạt động của Coderetreat được chứng minh là có hiệu quả cao, giúp cải thiện kỹ năng. Qua việc tập trung thực hành những nguyên tắc cơ bản của Thiết kế Đơn giản (simple design) và hướng đối tượng (OOP), các nhà phát triển có thể cải thiện trình độ, năng suất trong viết mã.</p>
+            </div>
+            </div>
+        </div>
+<div class="content">
+    <div class="facilitator">
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <h2>Facilitator</h2>
+    </div>
+            <div style="margin: 0 0 0 280px">
+                <img src="images/khoanv.png" style="width: 240px; height: 278px"/>
+                    <p style="color: orangered" class="facilitator-info">Nguyễn Việt Khoa</p>
+            </div>
+            <div style="margin: -333px 0 0 580px">
+                <img src="images/NhatNK.png" style="width: 240px; height: 278px"/>
+                    <p style="color: orangered">Nguyễn Khắc Nhật</p>
+            </div>
+            <div style="margin: -333px 0 0 880px">
+                <img src="images/sonnb.png" style="width: 240px; height: 278px"/>
+                    <p style="color: orangered">Nguyễn Bình Sơn</p>
+            </div>
+        <div>
+                <img src="images/bg5.png" style="width: 45%; height: 45%; margin: 65px 0 0 0 ">
+                <img src="images/coding.png" style="width: 35%; height: 35%; margin: 0 0 65px -470px">
+        </div>
+    <div class="table" style="margin: -550px 0 0 560px">
+        <div>
+            <h3 style="color: orangered">Agenda</h3>
+            <table>
+                <tbody>
+                    <tr>
+                        <td class="first-col">Lịch trình</td>
+                        <td>Chủ Nhật, 10/9/2017</td>
+                    </tr>
+                    <tr class="table-heading">
+                        <td class="first-col">Thời gian</td>
+                        <td>Nội dung</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">08:00 - 08:30</td>
+                        <td>Checkin, cafe sáng</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">08:30 - 09:00</td>
+                        <td>Khai mạc, giới thiệu codereatreat, trình bày kata</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">09:00 - 09:45</td>
+                        <td>Session #1</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">09:45 - 10:00</td>
+                        <td>retrospective, break</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">10:00 - 10:45</td>
+                        <td>Session #2</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">10:45 - 11:00</td>
+                        <td>retrospective, break</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">11:00 - 11:45</td>
+                        <td>Session #3</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">11:45 - 12:00</td>
+                        <td>retrospective, break</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">12:00 - 13:30</td>
+                        <td>Ăn trưa, nghỉ ngơi, cafe chém gió</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">13:30 - 14:15</td>
+                        <td>Session #4</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">14:15 - 14:30</td>
+                        <td>retrospective, break</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">14:30 - 15:15</td>
+                        <td>Session #5</td>
+                    </tr>
+                    <tr>
+                        <td class="first-col">15:15 - 16:00</td>
+                        <td>Grand retrospective, Bế mạc</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="notication">
+                <span style="color:orangered">Chuẩn bị: </span>
+                <span style="color:black">Ít nhất 1 laptop/2 người.</span>
+            </div>
+        </div>
+    </div>
+    <div class="interest" style="margin: 40px 0 0 0">
+            <div>
+                <img src="images/interest-1.png" style="width: 570px; height: 365px">
+                <div class="text" style="margin: 0 0 0 25px">
+                    <h4>Người chưa biết, chưa có kinh nghiệm</h4>
+                    <ul style="width: 35%">
+                        <li>Được trải nghiệm một ngày Thực hành Chủ ý (deliberate pratice, deep pratice) thông qua luyện tập các kỹ thuật căn bản trong lập trình: pair-programming, refactoring, TDD, Simple Design, ... </li>
+                        <li>Được giao lưu học hỏi từ người khác, người có kinh nghiệm hơn </li>
+                        <li>Được làm quen với những ngôn những ngôn ngữ mới </li>
+                        <li>và hơn thế nữa...</li>
+                    </ul>
+                </div>
+            </div>
+            <div style="margin: -613px 0 90px 583px">
+                <img src="images/interest-2.png" style="width: 570px; height: 365px">
+                    <h4>Người đã biết, đã có kinh nghiệm</h4>
+                    <ul style="width: 80%">
+                        <li>Được thoát khỏi công việc hằng ngày để thử nghiệm những ý tưởng mới </li>
+                        <li>Luyện tập để nâng tầm kỹ thuật mà mình đã biết</li>
+                        <li>Được giao lưu học hỏi, chia sẻ và thể hiện mình</li>
+                        <li>và hơn thế nữa...</li>
+                    </ul>
+            </div>
+    </div>
+
+        <center><a href="#" target="_blank" class="button">Đăng ký tham dự</a></center>
+    <br>
+        <center><img src="images/footer-background.png"/></center>
+</div>
+</body>
+</html>
